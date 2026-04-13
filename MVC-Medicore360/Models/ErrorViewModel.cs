3accdeec437f8efe721449e6969f23285de9ecc4
V <2483410@cognizant.com>
2026-04-13 10:00:18 +0530

    Initial structure commit

---
+++ MVC-Medicore360/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Medicore360.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
