ca77a6fa7bab6ca9a364a5bef2e405cd829ca123
JaswanthV005 <Viswanadhapalli.Jaswanth@cognizant.com>
2026-04-14 01:39:47 +0530

    added remaining tabel

---
+++ MVC-Medicore360/Models/KPIReport.cs
@@ -0,0 +1,10 @@
+namespace MediCore360.Models
+{
+    public class KPIReport
+    {
+        public int ReportID { get; set; }
+        public string Scope { get; set; }
+        public string? Metrics { get; set; }
+        public DateTime GeneratedDate { get; set; } = DateTime.UtcNow;
+    }
+}
